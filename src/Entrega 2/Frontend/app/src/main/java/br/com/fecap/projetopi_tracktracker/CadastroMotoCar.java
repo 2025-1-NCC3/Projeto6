85073ee8c8fed131a6349d40b55bc574b814bf4f
Nicolasrjm <nicolasrjm2@gmail.com>
2025-04-21 23:55:01 -0300

    add algumas telas do app

--- src/Entrega 2/Frontend/app/src/main/java/br/com/fecap/projetopi_tracktracker/CadastroMotoCar.java
+++ src/Entrega 2/Frontend/app/src/main/java/br/com/fecap/projetopi_tracktracker/CadastroMotoCar.java
@@ -1,66 +1,66 @@
-package br.com.fecap.projetopi_tracktracker;
+    package br.com.fecap.projetopi_tracktracker;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.view.View;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+    import androidx.activity.EdgeToEdge;
+    import androidx.appcompat.app.AppCompatActivity;
+    import androidx.core.graphics.Insets;
+    import androidx.core.view.ViewCompat;
+    import androidx.core.view.WindowInsetsCompat;
 
-public class CadastroMotoCar extends AppCompatActivity {
+    public class CadastroMotoCar extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_cadastro_moto_car);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            EdgeToEdge.enable(this);
+            setContentView(R.layout.activity_cadastro_moto_car);
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+        }
+
+        public void seguinteSenha(View view){
+            Intent telaSenha = new Intent(this, CadastroMotoSenha.class);
+            startActivity(telaSenha);
+        }
+
+        public void voltar(View view){
+            finish();
+        }
+
+        @Override
+        protected void onStart(){
+            super.onStart();
+        }
+
+        @Override
+        protected void onRestart(){
+            super.onRestart();
+        }
+
+        @Override
+        protected void onResume(){
+            super.onResume();
+        }
+
+        @Override
+        protected void onPause(){
+            super.onPause();
+        }
+
+        @Override
+        protected void onStop(){
+            super.onStop();
+        }
+
+        @Override
+        protected void onDestroy(){
+            super.onDestroy();
+        }
     }
-
-    public void seguinteSenha(View view){
-        Intent telaSenha = new Intent(this, CadastroMotoSenha.class);
-        startActivity(telaSenha);
-    }
-
-    public void voltar(View view){
-        finish();
-    }
-
-    @Override
-    protected void onStart(){
-        super.onStart();
-    }
-
-    @Override
-    protected void onRestart(){
-        super.onRestart();
-    }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-    }
-}
 
--- src/Entrega 2/Frontend/app/src/main/java/br/com/fecap/projetopi_tracktracker/CadastroPassSenha.java
+++ src/Entrega 2/Frontend/app/src/main/java/br/com/fecap/projetopi_tracktracker/CadastroPassSenha.java
@@ -10,22 +10,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginPass extends AppCompatActivity {
+public class CadastroPassSenha extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login_pass);
+        setContentView(R.layout.activity_cadastro_pass_senha);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        findViewById(R.id.btnSeguinte).setOnClickListener( v-> {
-            Intent intent = new Intent(LoginPass.this, HomePassageiro.class);
-            startActivity(intent);
-            finish();
         });
     }
 
@@ -33,38 +29,8 @@ public class LoginPass extends AppCompatActivity {
         finish();
     }
 
-    public void cadastro(View view){
-        Intent intent = new Intent(this, CadastroPassPessoal.class);
+    public void seguinteFoto(View view){
+        Intent intent = new Intent(this, CadastroPassSelfie.class);
         startActivity(intent);
-    }
-
-    @Override
-    protected void onStart(){
-        super.onStart();
-    }
-
-    @Override
-    protected void onRestart(){
-        super.onRestart();
-    }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
     }
 }
